eb8a5a1bd3c497d7432e76d7de06fdebf1d2940e
Archie <youkun@bilibili.com>
2023-02-27 16:02:47 +0800

    Initial Commit

---
+++ YEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YEditor
+//
+//  Created by 尤坤 on 2023/2/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
